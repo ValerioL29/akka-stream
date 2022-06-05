72920bd44c9ae62c163e60bffd48ebb73436725d
ValerioL29 <cheng2029@foxmail.com>
2022-06-06 01:17:20 +0800

    Equipped with Backpressure! - June 5th, 2022 - Add BackpressureBasics.scala

---
+++ src/main/scala/part2primer/4 - Backpressure.md
@@ -0,0 +1,13 @@
+# Backpressure
+
+> One of the fundamental features of ***Reactive Streams***
+
+Elements flow as response to ***demand*** from consumers.
+
+#### Fast consumers - all is well
+
+#### Slow consumers - problem
+
+Consumer will send a signal to producer to slow down.
+
+> Backpressure protocol is ***transparent***
---
+++ src/main/scala/part2primer/BackpressureBasics.scala
@@ -0,0 +1,71 @@
+package part2primer
+
+import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializer, OverflowStrategy}
+import akka.stream.scaladsl.{Flow, Sink, Source}
+
+import scala.language.postfixOps
+
+object BackpressureBasics extends App {
+  implicit val system: ActorSystem = ActorSystem("BackpressureBasics")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  val fastSource = Source(1 to 1000)
+  val slowSink = Sink.foreach[Int] { x: Int =>
+    // simulate a long processing
+    Thread.sleep(1000)
+    println(s"Sink: $x")
+  }
+
+  // fastSource.to(slowSink).run() // fusing?!
+  // not backpressure
+
+  // fastSource.async.to(slowSink).run()
+  // backpressure!
+
+  val simpleFlow = Flow[Int].map { x: Int =>
+    println(s"Incoming: $x")
+    x + 1
+  }
+
+  // Internal buffer is 16
+  // Buffering is one kind of Backpressure
+  //  fastSource.async
+  //    .via(simpleFlow).async
+  //    .to(slowSink)
+  //    .run()
+
+  /**
+   * Reactions to backpressure (in order):
+   * - try to slow down if possible
+   * - buffer elements until there's more demand
+   * - drop down elements from the buffer if it overflows
+   * - tear down/kill the whole stream (failure)
+   */
+  val bufferedFlow = simpleFlow.buffer(10, overflowStrategy = OverflowStrategy.dropHead)
+  fastSource.async
+    .via(bufferedFlow).async
+    .to(slowSink)
+    .run()
+
+  //  1 - 16: nobody is effected by Backpressure
+  //  17 - 26: flow will buffer, flow will start dropping at the next element
+  //  26 - 1000: flow will always drop the oldest elements
+  //     => 991 - 1000
+  //     => 992 - 1001
+  //     => sink
+
+  /**
+   * Overflow strategies:
+   * - drop head = oldest
+   * - drop tail = newest
+   * - drop new = exact element to be added = keeps the buffer
+   * - drop the entire buffer
+   * - backpressure signal
+   * - fail
+   */
+
+  // throttling
+  import scala.concurrent.duration._
+  fastSource.throttle(10, 1 second).runForeach(println)
+}
